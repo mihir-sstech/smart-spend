1f7b60b5b05858532ccf256f927449449f4038c4
Mihir <mihir.sstech@gmail.com>
2024-04-16 12:37:26 +0530

    👍

--- app/home/page.js
+++ app/home/page.js
@@ -94,7 +94,7 @@ const Header = () => {
                                     type="submit"
                                     onClick={() => scrollToSection('contact-us')} key="contact-us"
                                 >
-                                    HELP CENTER
+                                    HELP CENTERe
                                 </button>
                             </div>
                         </div>
