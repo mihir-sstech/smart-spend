a036e805229dc9f5dd66467d525989e575163557
Mihir <mihir.sstech@gmail.com>
2024-04-24 10:04:33 +0530

    👍

--- app/home/about.js
+++ app/home/about.js
@@ -36,10 +36,10 @@ const About = () => {
                         <p className="text-xl  text-gray-700 text-center md:text-base mb-4">
                             Introducing SmartSpend - Expense Manager, your ultimate solution for effortlessly managing your expenses and budget. Whether you&apos;re a meticulous planner or just looking to gain better control over your finances, SmartSpend is the go-to app for all your financial tracking needs.
                         </p>
-                        <p className="text-xl text-gray-700  font-medium text-center md:text-base mb-4">
+                        <p className="text-xl text-gray-700   text-center md:text-base mb-4">
                             Gone are the days of struggling to keep tabs on where your money goes. With SmartSpend, you can easily record and categorize your personal and business expenses, ensuring that every penny is accounted for. From groceries to utility bills, and from travel expenses to entertainment costs, SmartSpend helps you stay on top of it all with its intuitive interface and robust features.
                         </p>
-                        <p className="text-xl text-gray-700 text-center font-medium md:text-base mb-4">
+                        <p className="text-xl text-gray-700 text-center  md:text-base mb-4">
                             But SmartSpend isn&apos;t just about tracking expenses—it&apos;s also a powerful financial planning tool. Set budgets for different categories, track your spending against these budgets, and receive insightful reports to help you make informed decisions about your finances. With SmartSpend, you&apos;ll never exceed your budget unknowingly again.
                         </p>
                     </div>
